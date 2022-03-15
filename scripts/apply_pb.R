b1659d7802a1e60876d75211f633dbc2de089cc5
Gabriel Hoffman <gabriel.hoffman@gmail.com>
2022-03-14 20:35:17 -0400

    update

--- scripts/apply_pb.R
+++ scripts/apply_pb.R
@@ -2,6 +2,7 @@ suppressMessages({
     library(limma)
     library(muscat)
     library(scater)
+    library(dreamlet)
     library(sctransform)
     library(SingleCellExperiment)
 })
@@ -18,13 +19,31 @@ apply_pb <- function(sce, pars, ds_only = TRUE) {
         pb <- aggregateData(sce, a, fun = pars$fun, scale = pars$scale)
     })[[3]]
     t2 <- system.time({
-        res <- tryCatch(
-            do.call(pbDS, c(
-                list(pb = pb, filter = "none", verbose = FALSE),
-                pars[names(pars) %in% names(formals(pbDS))])),
-            error = function(e) e)
-        if (!inherits(res, "error"))
-            res <- dplyr::bind_rows(res$table[[1]])
+        if( pars$method == "dreamlet" ){
+            vobj <- processAssays(pb, ~ 1, verbose=FALSE)
+            fit <- dreamlet(vobj, ~ group_id, verbose=FALSE )
+            tab <- topTable(fit, coef='group_idB', number=Inf)
+
+            tab2 = with(tab, data.frame(gene = ID, cluster_id = assay, logFC, AveExpr, t, p_val=P.Value, B, contrast='B'))
+
+            tab2$p_adj.glb = p.adjust(tab2$p_val, "BH")
+            tab2$p_adj.loc = rep(NA, nrow(tab2))
+
+            for( CT in unique(tab2$cluster_id) ){
+                idx = which(tab2$cluster_id==CT)
+                tab2$p_adj.loc[idx] = p.adjust(tab2$p_val[idx], "BH")
+            }
+            res = tab2    
+        }else{
+            res <- tryCatch(
+                do.call(pbDS, c(
+                    list(pb = pb, filter = "none", verbose = FALSE),
+                    pars[names(pars) %in% names(formals(pbDS))])),
+                error = function(e) e)
+            if (!inherits(res, "error"))
+                res <- dplyr::bind_rows(res$table[[1]])
+        }
     })[[3]]
     list(rt = c(t1, t2), tbl = res)
 }
+
