2638632f7ff70b12df83d4ed7fb871ad20ac824e
Gabriel Hoffman <gabriel.hoffman@gmail.com>
2022-03-17 14:52:20 -0400

    update

--- scripts/run_pars.R
+++ scripts/run_pars.R
@@ -4,7 +4,12 @@ sim_ids <- jsonlite::fromJSON(config$sids)
 def_pars <- list(nr = 1, nk = "x", ns = "x", ng = "x", nc = "x", seed = 1)
 
 run_pars <- list(
-    kang = list(
+    # kang = list(
+    #     de10_ng = list(nr = 1, ng = c(500, 1e3, 2e3, 4e3)),
+    #     de10_nc = list(nr = 1, nc = c(50, 100, 200, 400)),
+    #     de10_ns = list(nr = 1, ns = c(2, 3, 4, 5))
+    # ),
+    nps = list(
         de10_ng = list(nr = 1, ng = c(500, 1e3, 2e3, 4e3)),
         de10_nc = list(nr = 1, nc = c(50, 100, 200, 400)),
         de10_ns = list(nr = 1, ns = c(2, 3, 4, 5))
