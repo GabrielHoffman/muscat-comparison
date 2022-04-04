2167e3b9126a6828e780c556b13564e7205686aa
Gabriel Hoffman <gabriel.hoffman@gmail.com>
2022-04-04 14:09:59 -0400

    update

--- scripts/utils.R
+++ scripts/utils.R
@@ -9,6 +9,8 @@ names(.cat_cols) <- c("ee", "ep", "de", "dp", "dm", "db")
     "edgeR.sum.scalecpm" = "#C6C6C6",
 
     "dreamlet.sum.counts" = "red",
+
+    "DESeq2.sum.counts" = "purple",
     
     "scDD.logcounts"    = "#0056B2",
     "scDD.vstresiduals" = "#009EF6", 
