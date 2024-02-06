a6f9d8fe8a9370ea61985714e1af68b592820c8d
Gabriel Hoffman <gabriel.hoffman@gmail.com>
2024-02-05 20:34:08 -0500

    update null

--- scripts/plot_null.R
+++ scripts/plot_null.R
@@ -18,7 +18,7 @@ df <- .read_res(args$res) %>%
     dplyr::rename(method = mid, replicate = i)
 
 p <- ggplot(df, aes(x = p_val, y = ..ndensity.., 
-    col = method, fill = method, lty = replicate)) +
+    col = method, fill = method)) + #, lty = replicate
     facet_wrap(~ method, ncol = 4) + 
     geom_density(adjust = 0.2, size = 0.3, alpha = 0.1) +
     scale_alpha_manual(values = c("TRUE" = 0.1, "FALSE" = 0.4)) +
@@ -28,9 +28,10 @@ p <- ggplot(df, aes(x = p_val, y = ..ndensity..,
         lty = guide_legend(ncol = 1, order = 2),
         fill = guide_legend(ncol = 1, order = 1,
             override.aes = list(alpha = 1, col = NA))) +
-    scale_x_continuous("p-value", breaks = seq(0, 1, 0.2), expand = c(0, 0.05)) +
-    scale_y_continuous("normalized density", breaks = c(0, 1), expand = c(0, 0.1)) +
-    .prettify("bw") + theme(aspect.ratio = 1/2,
+    scale_x_continuous("p-value", breaks = seq(0, 1), expand = c(0, 0.05)) +
+    scale_y_continuous("normalized density", breaks = c(0, 1), expand = c(0, 0)) +
+    .prettify("bw") + 
+    theme(aspect.ratio = 1,
         #legend.position = "bottom",
         legend.box.just = "left",
         panel.grid = element_blank(),
