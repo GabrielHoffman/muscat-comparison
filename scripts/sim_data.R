71c126cb667654f9a3898645aa159ed3deba6d89
Gabriel Hoffman <gabriel.hoffman@gmail.com>
2024-02-08 12:02:27 -0500

    more heterogen

--- scripts/sim_data.R
+++ scripts/sim_data.R
@@ -35,8 +35,9 @@ assignInNamespace( ".check_args_simData", function(u)
     # add expression heterogeneity to NB 
     minValue <- min(m)
     M = apply(m, 1, function(x){
-        rv_factor <- runif(1, 1, 1e6) # 0 is no additional 
-        x <- x + rv_factor*rnorm(length(x), 0, sd(x))
+        rv_factor <- runif(1, 1, 1e3) # 0 is no additional 
+        # x <- x + rv_factor*rnorm(length(x), 0, sd(x))
+        x * sqrt(rv_factor)
         pmax(x, minValue)
         })
     m <- m + t(M)
