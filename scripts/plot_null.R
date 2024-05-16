0892682fa60d15a7ebaeaed6956d3da23ff7d897
Gabriel Hoffman <gabriel.hoffman@gmail.com>
2024-05-16 11:15:45 -0400

    update null

--- scripts/plot_null.R
+++ scripts/plot_null.R
@@ -18,9 +18,11 @@ df <- .read_res(args$res) %>%
     dplyr::rename(method = mid, replicate = i)
 
 p <- ggplot(df, aes(x = p_val, y = ..ndensity.., 
-    col = method, fill = method, lty = replicate)) +
-    facet_wrap(~ method, ncol = 4) + 
-    geom_density(adjust = 0.2, size = 0.3, alpha = 0.1) +
+    col = method, fill = method)) +
+    # lty = replicate
+    facet_wrap(~ method, ncol = 3) + 
+    # geom_density(adjust = 0.2, size = 0.3, alpha = 0.1) +
+    geom_density(alpha = 0.4) +
     scale_alpha_manual(values = c("TRUE" = 0.1, "FALSE" = 0.4)) +
     scale_color_manual(values = .meth_cols) +
     scale_fill_manual(values = .meth_cols) +
@@ -30,16 +32,18 @@ p <- ggplot(df, aes(x = p_val, y = ..ndensity..,
             override.aes = list(alpha = 1, col = NA))) +
     scale_x_continuous("p-value", breaks = seq(0, 1, 0.2), expand = c(0, 0.05)) +
     scale_y_continuous("normalized density", breaks = c(0, 1), expand = c(0, 0.1)) +
-    .prettify("bw") + theme(aspect.ratio = 1,
-        #legend.position = "bottom",
-        legend.box.just = "left",
+    .prettify("bw") + 
+    # theme_classic() +
+    theme(aspect.ratio = 1,
+        legend.position = "none",
+        # legend.box.just = "left",
         panel.grid = element_blank(),
         panel.spacing = unit(1, "mm"),
         panel.border = element_rect(color = "grey"),
         strip.text = element_text(size = 5),
         axis.text.x = element_text(angle = 45, hjust = 1, vjust = 1))
 
-saveRDS(p, args$ggp)
+# saveRDS(p, args$ggp)
 ggsave(args$fig, p,
     units = "cm", width = 15, height = 8,
     dpi = 300, useDingbats = FALSE)
