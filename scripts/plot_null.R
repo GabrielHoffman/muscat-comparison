9e7676d4e7eb8d9b4edbbd2478bfcc8c19c4f2cb
Gabriel Hoffman <gabriel.hoffman@gmail.com>
2024-05-16 14:37:25 -0400

    update

--- scripts/plot_null.R
+++ scripts/plot_null.R
@@ -17,23 +17,19 @@ df <- .read_res(args$res) %>%
     dplyr::filter(!is.na(p_val)) %>% 
     dplyr::rename(method = mid, replicate = i)
 
-p <- ggplot(df, aes(x = p_val, y = ..ndensity.., 
+p <- ggplot(df, aes(x = p_val, y = after_stat(ndensity), 
     col = method, fill = method)) +
-    # lty = replicate
     facet_wrap(~ method, ncol = 3) + 
-    # geom_density(adjust = 0.2, size = 0.3, alpha = 0.1) +
     geom_density(alpha = 0.4) +
-    scale_alpha_manual(values = c("TRUE" = 0.1, "FALSE" = 0.4)) +
+    # scale_alpha_manual(values = c("TRUE" = 0.1, "FALSE" = 0.4)) +
     scale_color_manual(values = .meth_cols) +
     scale_fill_manual(values = .meth_cols) +
-    guides(col = FALSE,
-        lty = guide_legend(ncol = 1, order = 2),
+    guides(lty = guide_legend(ncol = 1, order = 2),
         fill = guide_legend(ncol = 1, order = 1,
             override.aes = list(alpha = 1, col = NA))) +
     scale_x_continuous("p-value", breaks = seq(0, 1, 0.2), expand = c(0, 0.05)) +
     scale_y_continuous("normalized density", breaks = c(0, 1), expand = c(0, 0.1)) +
     .prettify("bw") + 
-    # theme_classic() +
     theme(aspect.ratio = 1,
         legend.position = "none",
         # legend.box.just = "left",
@@ -43,7 +39,8 @@ p <- ggplot(df, aes(x = p_val, y = ..ndensity..,
         strip.text = element_text(size = 5),
         axis.text.x = element_text(angle = 45, hjust = 1, vjust = 1))
 
-# saveRDS(p, args$ggp)
+saveRDS(p, args$ggp)
+
 ggsave(args$fig, p,
     units = "cm", width = 15, height = 8,
     dpi = 300, useDingbats = FALSE)
