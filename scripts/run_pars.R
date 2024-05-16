1a8c3f73172978935bfc08048b1e9e41246bdc34
Gabriel Hoffman <gabriel.hoffman@gmail.com>
2024-05-15 20:48:10 -0400

    update

--- scripts/run_pars.R
+++ scripts/run_pars.R
@@ -5,7 +5,7 @@ def_pars <- list(nr = 1, nk = "x", ns = "x", ng = "x", nc = "x", seed = 1)
 
 run_pars <- list(
     kang = list(
-        # de10_ng = list(nr = 1, ng = c(500, 1e3, 2e3, 4e3)),
+        de10_ng = list(nr = 1, ng = c(4e3)),
         de10_nc = list(nr = 1, nc = c(50, 100, 400)),
         de10_ns = list(nr = 1, ns = c(10, 20, 30))
     )
